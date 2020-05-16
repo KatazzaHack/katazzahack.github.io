f71e3c90700b9ab0900134e187738c25e4eb92da
Olga Kalitova <olga.kalitova@gmail.com>
2020-05-16 11:36:31 +0300

    Initial game states handling

--- src/Game.js
+++ src/Game.js
@@ -4,15 +4,30 @@ import './Game.css';
 class Game extends React.Component {
 	constructor(props)	{
 		super(props);
-		console.log(this.props.is_start);
+		this.game_is_active = !this.props.is_start;
+		console.log('game active: ' + this.game_is_active);
 	}
 
-  render() { 
-		return (
-    	<div>
-				Game should be located here
-    	</div>
-  	);
+  render() {
+		if (this.game_is_active) {
+			return (
+    		<div>
+					Game should be located here
+    		</div>
+  		);
+		} else {
+			return (
+				<div>
+					<button onClick={this.startGame()}>
+						Start game
+					</button>
+				</div>
+			);
+		}
+	}
+
+	startGame() {
+		this.game_is_active = true;
 	}
 }
 
