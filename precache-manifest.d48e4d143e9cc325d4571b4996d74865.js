ac549a632279ff31d9e0c641ff43588c7e1277a6
Olga Kalitova <olga.kalitova@gmail.com>
2020-05-16 05:05:44 +0300

    Updates

---
+++ precache-manifest.d48e4d143e9cc325d4571b4996d74865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10c0e64efcb8b8b5f590dce3c87fac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9886165c866617eaef0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "01e97d104a10bae64172",
+    "url": "/static/js/2.e867de94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e867de94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9886165c866617eaef0",
+    "url": "/static/js/main.3c0ee2c5.chunk.js"
+  },
+  {
+    "revision": "7293ac03fee5545de4b5",
+    "url": "/static/js/runtime-main.f7129483.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
