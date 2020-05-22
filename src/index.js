feba7e8412c040b306a4ecd541679fb33aa6ac2f
antongulikov <contest.gulikov@gmail.com>
2020-05-22 21:34:48 +0200

    Add bootstrap to index.html

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import Content from './Content';
 import * as serviceWorker from './serviceWorker';
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <Content />
