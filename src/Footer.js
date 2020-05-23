5185ddc29d1e0e590b0bf336fa9f99d8732337f8
antongulikov <contest.gulikov@gmail.com>
2020-05-23 08:37:14 +0200

    add share

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Share from './Share';
 
 class Footer extends React.Component {
 
 	render() {
 		return (
 			<footer id="statfooter" class="page-footer font-small purple pt-4">
+			<Share
+      			title={"Вирусная игра"}
+      			url={`http://katazzahack.github.io/`}
+    		/>
         <div id="public-notice" class="container"><a href="https://www.freepik.com/free-photos-vectors/design">Design vector created by freepik - www.freepik.com</a></div>
+
 			</footer>
 		);
 	}
