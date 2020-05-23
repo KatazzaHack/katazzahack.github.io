56a9a15f55e52003a4134672197f5afe11c0b754
antongulikov <contest.gulikov@gmail.com>
2020-05-23 04:11:07 +0200

    Add costs

--- src/ClickTypeButtons.js
+++ src/ClickTypeButtons.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 class ClickTypeButtons extends React.Component {
 
@@ -21,14 +23,14 @@ class ClickTypeButtons extends React.Component {
 
 	render() {
 		return (<ButtonGroup size="sm">
-			{this.renderButton(0, "Себя", this.vari_type(0))} 
-			{this.renderButton(1, "+Первых соседей", this.vari_type(1))}
-			{this.renderButton(2, "+Вторых соседей", this.vari_type(2))}
+			{this.renderButton(0, "Себя", this.vari_type(0), 100)} 
+			{this.renderButton(1, "+Первых соседей", this.vari_type(1), 200)}
+			{this.renderButton(2, "+Вторых соседей", this.vari_type(2), 400)}
 		</ButtonGroup>);
 	}
 
-  renderButton(i, name, vari) {
-    return <ClickTypeButton button_name={name} variant={vari} onClick={() => this.handleClick(i)}/>;
+  renderButton(i, name, vari, cost) {
+    return <ClickTypeButton button_name={name} variant={vari} cost={cost} onClick={() => this.handleClick(i)}/>;
   }
 
   handleClick(i) {
@@ -39,11 +41,13 @@ class ClickTypeButtons extends React.Component {
 
 class ClickTypeButton extends React.Component {
 	render() {
-		return (
-			<Button variant={this.props.variant} onClick={() => this.props.onClick()}>
-        {this.props.button_name}
-			</Button>
-		);
+    return (
+      <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Это стоит {this.props.cost} рублей!</Tooltip>}>
+          <Button variant={this.props.variant} onClick={() => this.props.onClick()}>
+            {this.props.button_name}
+          </Button>
+      </OverlayTrigger>
+    );
 	}
 }
 
