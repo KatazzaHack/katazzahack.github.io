8798844fb230f38104b623fc60d89c99c0052e22
Olga Kalitova <olga.kalitova@gmail.com>
2020-05-23 04:10:55 +0300

    Updates

---
+++ precache-manifest.7b6d4e173e889b199d6961bd01ae2981.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca146ddeac6760c2da84cb6df628551c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb5d75b5969935d176a4",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7a250c492c8b12b4f735",
+    "url": "/static/css/main.b9460ed6.chunk.css"
+  },
+  {
+    "revision": "fb5d75b5969935d176a4",
+    "url": "/static/js/2.9a7c18b6.chunk.js"
+  },
+  {
+    "revision": "f205b902f40873790dab0ea40aba7440",
+    "url": "/static/js/2.9a7c18b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a250c492c8b12b4f735",
+    "url": "/static/js/main.f0d8fce7.chunk.js"
+  },
+  {
+    "revision": "64362da3800e49fe4593",
+    "url": "/static/js/runtime-main.1a65e5c7.js"
+  },
+  {
+    "revision": "c067a9fb46e25d608ddf852e920c69d6",
+    "url": "/static/media/mask2.c067a9fb.png"
+  },
+  {
+    "revision": "c6dba1dc57df8d07948461654f26c494",
+    "url": "/static/media/mask3.c6dba1dc.png"
+  },
+  {
+    "revision": "06cf95fb18c1ae7e9ce04773e666e971",
+    "url": "/static/media/nomask2.06cf95fb.png"
+  },
+  {
+    "revision": "321e13a62c57870b8c1ee7d2ec2e5bd6",
+    "url": "/static/media/vir1.321e13a6.png"
+  },
+  {
+    "revision": "8b37de42b8e944a2b2a7188300828b6b",
+    "url": "/static/media/vir2.8b37de42.png"
+  },
+  {
+    "revision": "0f420d3e048018e50c5bdbd374352c9d",
+    "url": "/static/media/vir3.0f420d3e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a8826747745be38f8aa235258f1df8e.js"
+  "/precache-manifest.7b6d4e173e889b199d6961bd01ae2981.js"
 );
 
 self.addEventListener('message', (event) => {
